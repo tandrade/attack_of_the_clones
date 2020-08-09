124607d074190a4ebce514bcc4f5819d85146bb0
Tamara Andrade <tandrade@stripe.com>
2020-08-08 21:41:03 -0500

    Fleshing out models

--- patreon/patreon/src/models.py
+++ patreon/patreon/src/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class PatreonModel(models.Model): 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta: 
+        abstract = True
+
+class Person(PatreonModel): 
+    pass 
+
+class Page(PatreonModel): 
+    owners = models.ManyToManyField(Person) 
+
+class Update(PatreonModel): 
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+    content = models.TextField()
+
+class Attachments(PatreonModel): 
+    uploaded = models.FileField() 
+    update = models.ForeignKey(Update, on_delete=models.CASCADE)
